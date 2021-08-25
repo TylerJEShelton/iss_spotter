705a72293f524c8d4c2b2a06393432e527862ef2
Tyler <tylershelton99@gmail.com>
2021-08-25 03:05:08 +0000

    Complete ISS Spotter I Assignment

---
+++ iss.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+const fetchMyIP = callback => {
+  // use request to fetch IP address from JSON API
+  const url = "https://api.ipify.org?format=json";
+
+  request(url, (err, response, body) => {
+    if (err) {
+      callback(`There was an error: ${err}`, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const ip = data.ip;
+    callback(null, ip);
+  });
+
+};
+
+module.exports = { fetchMyIP };
