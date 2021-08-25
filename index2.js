2926f404ca5c0e7fc07a31151394193c2f3e031d
Tyler <tylershelton99@gmail.com>
2021-08-25 06:25:23 +0000

    correct linting issue

--- index2.js
+++ index2.js
@@ -1,7 +1,5 @@
 const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
 
 fetchMyIP()
-  //.then(ip => console.log('It worked! Returned IP:', ip));
   .then(ip => fetchCoordsByIP(ip))
-  .then(coords => console.log(coords))
-    //.then()
+  .then(coords => console.log(coords));
